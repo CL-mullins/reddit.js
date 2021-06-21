ffd260bfa7623f4ffbb6c49a3d3e2dea9ba968a0
Christopher Mullins <cmullins318@gmail.com>
2021-06-21 15:15:57 -0700

    users can add a post

--- server.js
+++ server.js
@@ -2,12 +2,22 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000
+const Post = require
+
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
   res.render('home')
+})
+
+app.get('/posts/new', (req, res) => {
+  res.render('posts-new')
+})
+
+app.get('posts/create', (req, rest) => {
+  res.render('/')
 })
 
 app.listen(port, () => {
