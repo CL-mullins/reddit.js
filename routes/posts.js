52a503c0a408dd7ea544242f2cc847afe0de968e
Christopher Mullins <cmullins318@gmail.com>
2021-06-24 13:36:31 -0700

    users can NOW view posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,9 +1,37 @@
-var express = require('express');
-var router = express.Router();
+const Post = require('../models/post');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.render('posts-new', {title: 'Express'});
-});
+module.exports = (app) => {
 
-module.exports = router;
+  //  INDEX
+  app.get('/', (req, res) => {
+    Post.find({}).lean()
+      .then((posts) => res.render('posts-index', { posts }))
+      .catch((err) => {
+        console.log(err.message);
+      })
+  })
+
+    // NEW
+  app.get('/posts/new', (req, res) => {
+    res.render('posts-new')
+  });
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB AND REDIRECT TO THE ROOT
+    post.save(() => res.redirect('/'));
+  });
+
+    // LOOK UP THE POST
+  app.get('/posts/:id', (req, res) => {
+    Post.findById(req.params.id).lean()
+      .then((post) => res.render('posts-show', { post }))
+      .catch((err) => {
+        console.log(err.message);
+      });
+  });
+};
+
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express')
 const exphbs = require('express-handlebars');
 const app = express()
 const port = 3000
-require('./controllers/posts')(app);
 
+// MIDDLEWEAR
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
@@ -11,32 +11,13 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// Set db
+// DATABASE
 require('./data/reddit-db');
 
-// Middlewear above
 
-const Post = require('./models/post');
 
-//////////////////////////////////
-//         Routes               //
-//////////////////////////////////
+require('./routes/posts')(app);
 
-app.get('/', (req, res) => {
-  Post.find({}).lean()
-    .then((posts) => res.render('posts-index', { posts }))
-    .catch((err) => {
-      console.log(err.message);
-    })
-})
-
-app.get('/posts/new', (req, res) => {
-  res.render('posts-new')
-})
-
-app.get('posts/create', (req, rest) => {
-  res.render('/')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
