10d11b233e3af5f1f60a0c130ddb759e3ef62c8e
Christopher Mullins <cmullins318@gmail.com>
2021-06-28 15:16:00 -0700

    implemented remaining authentication tests

--- test/auth.js
+++ test/auth.js
@@ -32,6 +32,19 @@ describe("User", function () {
           done();
         });
     });
+
+    // login
+    it('should be able to login', function (done) {
+        agent
+        .post('/login')
+        .send({ username: 'testone', password: 'password' })
+        .end(function (err, res) {
+            res.should.have.status(200);
+            agent.should.have.cookie('nToken');
+            done();
+        });
+    });
+
   });
   after(function () {
     agent.close();
